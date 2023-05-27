4c0c9e66c8419915c61553aa789bbc9b98653a27
Kommandor <a.tsymbaliuk2007@gmail.com>
2023-05-27 09:42:16 +0300

    3

--- dz28.05.py
+++ dz28.05.py
@@ -1,15 +1,14 @@
-##2
-def average_closure():
-    numbers = []
-    def add_number(number):
-        numbers.append(number)
-    def get_avg():
-        if not numbers:
-            return 0
-        return sum(numbers) / len(numbers)
-    return add_number , get_avg
-add_num , get_average = average_closure()
-add_num(10)
-add_num(5)
-add_num(100)
-print(get_average())
+##3
+import random
+class PasswordGenerator:
+    def __init__(self,length,characters):
+        self.length = length
+        self.characters = characters
+
+    def generate_password(self):
+        password = ''.join(random.choice(self.characters) for i in range(self.length))
+        return password
+password_gen = PasswordGenerator(length = 10 , characters = "aftrwddt455@#!%^%))_*SJHfdge")
+print(password_gen.generate_password())
+print(password_gen.generate_password())
+print(password_gen.generate_password())
